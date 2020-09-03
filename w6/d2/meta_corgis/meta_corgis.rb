ef83cc8b3b96b7a33a3351fcff8458c5c1cd3299
kevinsu <kevin.suboy@gmail.com>
2020-09-02 18:05:23 -0700

    w6d2/w6d3 hmk

---
+++ w6/d2/meta_corgis/meta_corgis.rb
@@ -0,0 +1,162 @@
+require 'byebug'
+class SnackBox
+  SNACK_BOX_DATA = {
+    1 => {
+      "bone" => {
+        "info" => "Phoenician rawhide",
+        "tastiness" => 20
+      },
+      "kibble" => {
+        "info" => "Delicately braised hamhocks",
+        "tastiness" => 33
+      },
+      "treat" => {
+        "info" => "Chewy dental sticks",
+        "tastiness" => 40
+      }
+    },
+    2 => {
+      "bone" => {
+        "info" => "An old dirty bone",
+        "tastiness" => 2
+      },
+      "kibble" => {
+        "info" => "Kale clusters",
+        "tastiness" => 1
+      },
+      "treat" => {
+        "info" => "Bacon",
+        "tastiness" => 80
+      }
+    },
+    3 => {
+      "bone" => {
+        "info" => "A steak bone",
+        "tastiness" => 64
+      },
+      "kibble" => {
+        "info" => "Sweet Potato nibbles",
+        "tastiness" => 45
+      },
+      "treat" => {
+        "info" => "Chicken bits",
+        "tastiness" => 75
+      }
+    }
+  }
+  def initialize(data = SNACK_BOX_DATA)
+    @data = data
+  end
+
+  def get_bone_info(box_id)
+    @data[box_id]["bone"]["info"]
+  end
+
+  def get_bone_tastiness(box_id)
+    @data[box_id]["bone"]["tastiness"]
+  end
+
+  def get_kibble_info(box_id)
+    @data[box_id]["kibble"]["info"]
+  end
+
+  def get_kibble_tastiness(box_id)
+    @data[box_id]["kibble"]["tastiness"]
+  end
+
+  def get_treat_info(box_id)
+    @data[box_id]["treat"]["info"]
+  end
+
+  def get_treat_tastiness(box_id)
+    @data[box_id]["treat"]["tastiness"]
+  end
+end
+
+class CorgiSnacks
+
+  def initialize(snack_box, box_id)
+    @snack_box = snack_box
+    @box_id = box_id
+  end
+
+  def bone
+    info = @snack_box.get_bone_info(@box_id)
+    tastiness = @snack_box.get_bone_tastiness(@box_id)
+    result = "Bone: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
+  end
+
+  def kibble
+    info = @snack_box.get_kibble_info(@box_id)
+    tastiness = @snack_box.get_kibble_tastiness(@box_id)
+    result = "Kibble: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
+  end
+
+  def treat
+    info = @snack_box.get_treat_info(@box_id)
+    tastiness = @snack_box.get_treat_tastiness(@box_id)
+    result = "Treat: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
+  end
+
+end
+
+
+class MetaCorgiSnacks
+  def initialize(snack_box, box_id)
+    @snack_box = snack_box
+    @box_id = box_id
+
+    @snack_box.methods.grep(/^get_(.*)_info$/) { MetaCorgiSnacks.define_snack $1 }
+  end
+
+  # def method_missing(name, *args)
+  #   # Your code goes here...
+  #   # p name
+  #   # p *args
+  #   # debugger
+  #   n = @snack_box.send("get_#{name}_info", @box_id)
+  #   t = @snack_box.send("get_#{name}_tastiness", @box_id)
+  #   # p name
+  #   display_name = "#{name.to_s.split('_').map(&:capitalize).join(' ')}"
+  #   # p name
+  #   result = "#{display_name}: #{n}: #{t} "
+  #   t > 30 ? "* #{result}" : result
+
+  # end
+
+
+  def self.define_snack(name)
+    # Your code goes here...
+    define_method(name) do
+      n = @snack_box.send("get_#{name}_info", @box_id)
+      t = @snack_box.send("get_#{name}_tastiness", @box_id)
+      display_name = "#{name.split('_').map(&:capitalize).join(' ')}"
+      result = "#{display_name}: #{n}: #{t} "
+      t > 30 ? "* #{result}" : result
+    end
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  # snack_box = SnackBox.new
+  # meta_snacks = MetaCorgiSnacks.new(snack_box, 1)
+  # p meta_snacks.bone # => "Bone: Phoenician rawhide: 20 "
+  # p meta_snacks.kibble # => "* Kibble: Delicately braised hamhocks: 33"
+
+  # MetaCorgiSnacks.define_snack("bone")
+  # snack_box = SnackBox.new
+  # meta_snacks = MetaCorgiSnacks.new(snack_box, 1)
+  # p meta_snacks.bone # => "Bone: Phoenician rawhide: 20 "
+  # p meta_snacks.kibble # => "NoMethodError: undefined method `kibble'...""
+
+  snack_box = SnackBox.new
+  meta_snacks = MetaCorgiSnacks.new(snack_box, 1)
+  p meta_snacks.bone # => "Bone: Phoenician rawhide: 20 "
+  p meta_snacks.kibble # => "* Kibble: Delicately braised hamhocks: 33 "
+  p meta_snacks.treat # => "Treat: Chewy dental sticks: 40 "
+
+end
